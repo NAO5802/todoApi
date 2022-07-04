096eb7a9422409a20d8abe605d8d1d4b52c4748f
NAO5802 <i100185b@gmail.com>
2022-07-04 23:55:53 +0900

    add integration test of post request

--- src/test/kotlin/com/example/todoApi/todoApi/controller/TaskControllerTest.kt
+++ src/test/kotlin/com/example/todoApi/todoApi/controller/TaskControllerTest.kt
@@ -3,12 +3,13 @@ package com.example.todoApi.todoApi.controller
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.http.HttpStatus
+import java.util.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class TaskControllerTest(
@@ -17,13 +18,35 @@ internal class TaskControllerTest(
 
     @Test
     fun `pingにリクエストするとpongの文字列が返る`(){
-        val response = restTemplate.getForEntity<String>("/ping")
-        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(response.body).isEqualTo("pong")
+        val actual = restTemplate.getForEntity<String>("/ping")
+
+        assertThat(actual.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(actual.body).isEqualTo("pong")
     }
 
     @Test
-    fun `postTask - 新規タスクを作成できること`() {
+    fun `postTask - 新規タスクを作成できること(全ての項目あり)`() {
+        val request = TaskRequest("買い出し", "TODO", "豚肉200g", "花子")
+        val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
 
+        assertThat(actual.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(actual.body?.id).isInstanceOf(String::class.java)
+        assertThat(actual.body?.name).isEqualTo("買い出し")
+        assertThat(actual.body?.status).isEqualTo("TODO")
+        assertThat(actual.body?.description).isEqualTo("豚肉200g")
+        assertThat(actual.body?.createdBy).isEqualTo("花子")
+    }
+
+    @Test
+    fun `postTask - 新規タスクを作成できること(説明文なし)`() {
+        val request = TaskRequest("買い出し", "TODO", null, "花子")
+        val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
+
+        assertThat(actual.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(actual.body?.id).isInstanceOf(String::class.java)
+        assertThat(actual.body?.name).isEqualTo("買い出し")
+        assertThat(actual.body?.status).isEqualTo("TODO")
+        assertThat(actual.body?.description).isNull()
+        assertThat(actual.body?.createdBy).isEqualTo("花子")
     }
 }
