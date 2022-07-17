a953d3dff32bcedf7212f7feb2a100c098785b3a
NAO5802 <i100185b@gmail.com>
2022-07-17 17:53:58 +0900

    make usecase transactional

--- src/main/kotlin/com/example/todoApi/todoApi/usecase/TaskUseCase.kt
+++ src/main/kotlin/com/example/todoApi/todoApi/usecase/TaskUseCase.kt
@@ -3,10 +3,12 @@ package com.example.todoApi.todoApi.usecase
 import com.example.todoApi.todoApi.domain.Task
 import com.example.todoApi.todoApi.domain.TaskRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 // TODO: ServiceとComponentの違いを理解する
 @Service
 class TaskUseCase(val repository: TaskRepository) {
 
+    @Transactional
     fun create(task: Task): Task = repository.create(task)
 }
