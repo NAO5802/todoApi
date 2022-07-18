aaf65a738c57e8ca86e00006e4422b0e6ea8d1d1
NAO5802 <i100185b@gmail.com>
2022-07-18 17:45:47 +0900

    gen schema by jooq

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.rohanprabhu.gradle.plugins.kdjooq.*
 
 plugins {
 	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+    id("com.rohanprabhu.kotlin-dsl-jooq") version "0.4.6"
 }
 
 group = "com.example.todoApi"
@@ -25,7 +27,33 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.ninja-squad:springmockk:3.1.1")
-	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql:42.3.2")
+    jooqGeneratorRuntime("org.postgresql:postgresql:42.3.2")
+}
+
+jooqGenerator {
+    configuration("primary", project.the<SourceSetContainer>()["main"]) {
+        configuration = jooqCodegenConfiguration {
+            jdbc {
+                username = "postgres"
+                password = "docker"
+                driver = "org.postgresql.Driver"
+                url = "jdbc:postgresql://localhost:5433/todo"
+            }
+
+            generator {
+                target {
+                    packageName = "com.example.todoApi.todoApi.driver.gen"
+                    directory = "${project.rootDir}/src/main/kotlin"
+                }
+
+                database {
+                    name = "org.jooq.meta.postgres.PostgresDatabase"
+                    inputSchema = "public"
+                }
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
