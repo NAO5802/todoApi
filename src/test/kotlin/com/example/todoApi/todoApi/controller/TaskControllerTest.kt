7f7949027afdd464d3475ff9dd68da69b4b362b9
NAO5802 <i100185b@gmail.com>
2022-07-31 17:19:31 +0900

    nest related tests

--- src/test/kotlin/com/example/todoApi/todoApi/controller/TaskControllerTest.kt
+++ src/test/kotlin/com/example/todoApi/todoApi/controller/TaskControllerTest.kt
@@ -5,6 +5,7 @@ import com.example.todoApi.todoApi.TaskTestFactory
 import com.example.todoApi.todoApi.controller.task.TaskRequest
 import com.example.todoApi.todoApi.controller.task.TaskResponse
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -29,94 +30,106 @@ internal class TaskControllerTest(
         assertThat(actual.body).isEqualTo("pong")
     }
 
-    @Test
-    fun `postTask - 新規タスクを作成できること(全ての項目あり)`() {
-        val request = TaskRequest("買い出し", "TODO", "豚肉200g", "花子")
-        val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
+    @Nested
+    inner class PostTaskTest(){
 
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.CREATED)
-        assertThat(actual.body?.id).isInstanceOf(String::class.java)
-        assertThat(actual.body?.name).isEqualTo("買い出し")
-        assertThat(actual.body?.status).isEqualTo("TODO")
-        assertThat(actual.body?.description).isEqualTo("豚肉200g")
-        assertThat(actual.body?.createdBy).isEqualTo("花子")
+        @Test
+        fun `postTask - 新規タスクを作成できること(全ての項目あり)`() {
+            val request = TaskRequest("買い出し", "TODO", "豚肉200g", "花子")
+            val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
+
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.CREATED)
+            assertThat(actual.body?.id).isInstanceOf(String::class.java)
+            assertThat(actual.body?.name).isEqualTo("買い出し")
+            assertThat(actual.body?.status).isEqualTo("TODO")
+            assertThat(actual.body?.description).isEqualTo("豚肉200g")
+            assertThat(actual.body?.createdBy).isEqualTo("花子")
+        }
+
+        @Test
+        fun `postTask - 新規タスクを作成できること(説明文なし)`() {
+            val request = TaskRequest("買い出し", "TODO", null, "花子")
+            val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
+
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.CREATED)
+            assertThat(actual.body?.id).isInstanceOf(String::class.java)
+            assertThat(actual.body?.name).isEqualTo("買い出し")
+            assertThat(actual.body?.status).isEqualTo("TODO")
+            assertThat(actual.body?.description).isNull()
+            assertThat(actual.body?.createdBy).isEqualTo("花子")
+        }
+
+        @Test
+        fun `postTask - リクエストの値が不正な場合、400エラーを返す`() {
+            val request = TaskRequest("", "AAAAAA", null, "")
+            val actual = restTemplate.postForEntity<ErrorResponse>("/tasks", request)
+
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        }
     }
 
-    @Test
-    fun `postTask - 新規タスクを作成できること(説明文なし)`() {
-        val request = TaskRequest("買い出し", "TODO", null, "花子")
-        val actual = restTemplate.postForEntity<TaskResponse>("/tasks", request)
+    @Nested
+    inner class FindTaskTest(){
 
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.CREATED)
-        assertThat(actual.body?.id).isInstanceOf(String::class.java)
-        assertThat(actual.body?.name).isEqualTo("買い出し")
-        assertThat(actual.body?.status).isEqualTo("TODO")
-        assertThat(actual.body?.description).isNull()
-        assertThat(actual.body?.createdBy).isEqualTo("花子")
+        @Test
+        fun `findTask - 指定したIDのタスクが返ること`() {
+            // given
+            val taskId = taskTestDataCreator.create(TaskTestFactory.create())
+
+            // when
+            val actual = restTemplate.getForEntity<TaskResponse>("/tasks/${taskId.value}")
+
+            // then
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.OK)
+            assertThat(actual.body?.id).isEqualTo(taskId.value.toString())
+            assertThat(actual.body?.name).isEqualTo("買い物")
+            assertThat(actual.body?.status).isEqualTo("TODO")
+            assertThat(actual.body?.description).isEqualTo("ひき肉を買う")
+            assertThat(actual.body?.createdBy).isEqualTo("TODO太郎")
+        }
+
+        @Test
+        fun `findTask - 指定したIDの形式が不正の場合、400エラーを返す`() {
+            val actual = restTemplate.getForEntity<ErrorResponse>("/tasks/1234567")
+
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        }
+
+        @Test
+        fun `findTask - 指定したIDが存在しない場合、404エラーを返す`() {
+            val actual = restTemplate.getForEntity<ErrorResponse>("/tasks/95ec62c1-ac8a-4888-bfeb-059f0d648ef6")
+
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        }
     }
 
-    @Test
-    fun `postTask - リクエストの値が不正な場合、400エラーを返す`() {
-        val request = TaskRequest("", "AAAAAA", null, "")
-        val actual = restTemplate.postForEntity<ErrorResponse>("/tasks", request)
+    @Nested
+    inner class DeleteTaskTest(){
 
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
-    }
+        @Test
+        fun `deleteTask - 指定したIDのタスクが削除できること`() {
+            // given
+            val taskId = taskTestDataCreator.create(TaskTestFactory.create())
 
-    @Test
-    fun `findTask - 指定したIDのタスクが返ること`() {
-        // given
-        val taskId = taskTestDataCreator.create(TaskTestFactory.create())
+            // when
+            val actual = restTemplate.exchange<HttpStatus>("/tasks/${taskId.value}", HttpMethod.DELETE)
 
-        // when
-        val actual = restTemplate.getForEntity<TaskResponse>("/tasks/${taskId.value}")
+            // then
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.NO_CONTENT)
+        }
 
-        // then
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(actual.body?.id).isEqualTo(taskId.value.toString())
-        assertThat(actual.body?.name).isEqualTo("買い物")
-        assertThat(actual.body?.status).isEqualTo("TODO")
-        assertThat(actual.body?.description).isEqualTo("ひき肉を買う")
-        assertThat(actual.body?.createdBy).isEqualTo("TODO太郎")
-    }
+        @Test
+        fun `deleteTask - 指定したIDの形式が不正の場合、400エラーを返す`() {
+            val actual = restTemplate.exchange<Unit>("/tasks/1234567", HttpMethod.DELETE)
 
-    @Test
-    fun `findTask - 指定したIDの形式が不正の場合、400エラーを返す`() {
-        val actual = restTemplate.getForEntity<ErrorResponse>("/tasks/1234567")
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        }
 
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
-    }
+        @Test
+        fun `deleteTask - 指定したIDが存在しない場合、404エラーを返す`() {
+            val actual = restTemplate.exchange<Unit>("/tasks/95ec62c1-ac8a-4888-bfeb-059f0d648ef6", HttpMethod.DELETE)
 
-    @Test
-    fun `findTask - 指定したIDが存在しない場合、404エラーを返す`() {
-        val actual = restTemplate.getForEntity<ErrorResponse>("/tasks/95ec62c1-ac8a-4888-bfeb-059f0d648ef6")
-
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
-    }
-
-    @Test
-    fun `deleteTask - 指定したIDのタスクが削除できること`() {
-        // given
-        val taskId = taskTestDataCreator.create(TaskTestFactory.create())
-
-        // when
-        val actual = restTemplate.exchange<HttpStatus>("/tasks/${taskId.value}", HttpMethod.DELETE)
-
-        // then
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.NO_CONTENT)
-    }
-
-    @Test
-    fun `deleteTask - 指定したIDの形式が不正の場合、400エラーを返す`() {
-        val actual = restTemplate.exchange<Unit>("/tasks/1234567", HttpMethod.DELETE)
-
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
-    }
-
-    @Test
-    fun `deleteTask - 指定したIDが存在しない場合、404エラーを返す`() {
-        val actual = restTemplate.exchange<Unit>("/tasks/95ec62c1-ac8a-4888-bfeb-059f0d648ef6", HttpMethod.DELETE)
-
-        assertThat(actual.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+            assertThat(actual.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+        }
     }
 }
